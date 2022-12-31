04f0b04c95dee21b71b67c5b25d30f9015e033b1
ikhsanse <ikhsansolihin49@gmail.com>
2022-12-31 10:46:18 +0700

    code fixing availmeals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -9,28 +9,24 @@ const DUMMY_MEALS = [
     name: "Sushi",
     description: "Finest fish and veggies",
     price: 65505,
-    string_price : '65.505' 
   },
   {
     id: "m2",
     name: "Schnitzel",
     description: "A german specialty!",
     price: 76522,
-    string_price :"76.522"
   },
   {
     id: "m3",
     name: "Barbecue Burger",
     description: "American, raw, meaty",
     price: 52999,
-    string_price : '52.999',
   },
   {
     id: "m4",
     name: "Gado Gado",
     description: "Healthy...and green...",
     price: 20999,
-    string_price : '20.999'
   },
 ];
 
@@ -42,7 +38,6 @@ const AvailableMeals = () => {
       name={meal.name}
       desc={meal.description}
       price={meal.price}
-      str_price={meal.string_price}
     />
   ));
   return (
